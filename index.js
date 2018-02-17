42375fd7b4826e38e9d5c6b3c336c29966a5bd92
Peter Seremetis <pswriter5348@gmail.com>
2018-02-17 02:43:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 //The function establishes the arrays, musicians and instruments, as the parameters.
 function theBeatlesPlay(musicians, instruments){
   var blankArray = []
-//when the counter variable i is between 0 and the length of the musicians array (in this case 4), the loop will insert the elements from [musicians] and [instruments] into the variable: blank array. 
+//when the counter variable i is between 0 and the length of the musicians array (in this case 4), the loop will insert the elements from [musicians] and [instruments] into the variable: blank array.
     for (let i = 0; i < musicians.length; i++)
        blankArray [i] = (`${musicians[i]} plays ${instruments[i]}`)
 //Once the loop is run, the program will return the new array. 
   return blankArray
    }
-//The function establishes an array (any array) as the parameter. 
+//The function establishes an array (any array) as the parameter.
 function johnLennonFacts(array){
 //When the counter variable i is between 0 and the array length, the program will loop through each index of the array, and insert "!!!" at the end of each element of the array.
      let i = 0;
@@ -15,15 +15,18 @@ function johnLennonFacts(array){
        array[i] = (array[i]+ `!!!`);
        i++;
     }
-//Once the loop is run, the program will return the new array. 
+//Once the loop is run, the program will return the new array.
     return array
 }
+//The function establishes a number (any number) as the parameter. 
 function iLoveTheBeatles(aNumber){
   var loveBeatlesArray = [];
+//When the counter variable i is between the given number and 15, the program will insert the string "I love the Beatles!" into the variable, loveBeatlesArray. 
   let i = aNumber;
   do {
     loveBeatlesArray.push(`I love the Beatles!`);
     i++;
   } while (i < 15);
+//Once the loop is run, the program will return the new array. 
   return loveBeatlesArray
 }
